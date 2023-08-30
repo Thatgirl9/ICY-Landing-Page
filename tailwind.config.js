1f8b1fd41063c7a8648954bddcad8391e61afb5b
Favour Ukonu <favouroukonu@gmail.com>
2023-08-30 03:30:21 +0100

    completed-section-one

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,17 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
+      colors: {
+        textColorBlack: "rgba(0, 0, 0, 1)",
+        textColorBlue: "rgba(33, 80, 254, 1)",
+      },
+
       fontFamily: {
-        body: [],
+        body: ['"Hanken Grotesk"', "sans-serif"],
+      },
+
+      backgroundImage: {
+        sectionOneDiv2: "url('/Section-One-Assets/Rectangle 1.png')",
       },
     },
   },
